a8e192364754922562e7866f4b2a06f5bfb6e386
Duleendra Shashimal <duleendra@bambu.co>
2022-04-09 23:51:25 +0800

    Added missing files

---
+++ sample-ecs-services/customer-service/server.js
@@ -0,0 +1,42 @@
+'use strict';
+import fetch from 'node-fetch';
+import express from 'express'
+
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+const INTERNAL_ALB='service.internal'
+//const INTERNAL_ALB = '127.0.0.1:3000';
+
+// App
+const app = express();
+app.get('/health', (req, res) => {
+    res.status(200).send('Ok');
+});
+
+app.get('/customers/:customerId', (req, res) => {
+    let customerId = req.params['customerId']
+    let customerName = 'Nick John'
+    getCustomerAccountInfo(req,res,customerId);
+});
+
+async function getCustomerAccountInfo (req, res, customerId) {
+    let response;
+    try {
+        response = await fetch(`http://${INTERNAL_ALB}/accounts/customer/${customerId}`)  ;
+        let accounts = await response.text();
+        res.status(200).json({
+            id: customerId,
+            name: 'John',
+            accounts: JSON.parse(accounts)
+        });
+    } catch (err) {
+        console.log('Http error', err);
+        return res.status(500).send();
+    }
+
+}
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
